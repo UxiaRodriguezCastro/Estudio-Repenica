b72d28dbd308f48b693da95e72650da560b140ce
UxiaRodriguezCastro <uxiacastro@gmail.com>
2023-10-02 22:06:50 +0200

    Gallego default

--- css/js/script.js
+++ css/js/script.js
@@ -1,56 +1,56 @@
-	
-	let fullImgBox, fullImg, titulo, bio, fullImgBox2, titulo2, bio2, x, idioma, url;
-	
-	document.addEventListener("DOMContentLoaded", function () {
-	
-	
-	 fullImgBox= document.getElementById("fullimgbox");
-	 fullImgBox2= document.getElementById("fullimgbox2");
-     fullImg= document.getElementById("fullimagen");
-	 titulo= document.getElementById("nombreProfe");
-	 bio= document.getElementById("bioProfe");
-	 titulo2= document.getElementById("nombreProfe2");
-	 bio2= document.getElementById("bioProfe2");
-	 x = document.getElementById("myTopnav");	
-	 url = window.location.pathname;
-    });
-	
-	
-	function openFullImg(reference){
-	  fullImgBox.style.display = "flex";
-	  fullImg.src = reference
+
+let fullImgBox, fullImg, titulo, bio, fullImgBox2, titulo2, bio2, x, idioma, url;
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+	fullImgBox = document.getElementById("fullimgbox");
+	fullImgBox2 = document.getElementById("fullimgbox2");
+	fullImg = document.getElementById("fullimagen");
+	titulo = document.getElementById("nombreProfe");
+	bio = document.getElementById("bioProfe");
+	titulo2 = document.getElementById("nombreProfe2");
+	bio2 = document.getElementById("bioProfe2");
+	x = document.getElementById("myTopnav");
+	url = window.location.pathname;
+});
+
+
+function openFullImg(reference) {
+	fullImgBox.style.display = "flex";
+	fullImg.src = reference
+}
+
+function openFullScreen(nombre, biografia) {
+	fullImgBox.style.display = "block";
+	titulo.innerText = nombre;
+	bio.innerText = biografia;
+}
+
+function openFullScreen2(nombre, biografia) {
+	fullImgBox2.style.display = "block";
+	titulo2.innerText = nombre;
+	bio2.innerText = biografia;
+}
+
+function closeImg() {
+	fullImgBox.style.display = "none";
+	fullImgBox2.style.display = "none";
+}
+
+function menuFunction() {
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
 	}
+}
 
-	function openFullScreen(nombre, biografia){
-	  fullImgBox.style.display = "block";
-	  titulo.innerText= nombre;
-	  bio.innerText= biografia;
-	}	
-	
-	function openFullScreen2(nombre, biografia){
-		fullImgBox2.style.display = "block";
-		titulo2.innerText= nombre;
-		bio2.innerText= biografia;
-	  }	
-	
-	function closeImg(){
-	  fullImgBox.style.display = "none";
-	  fullImgBox2.style.display = "none";
-	}	
+function countryHandler(value) {
+	if (value == "español") {
+		location.href = "es/" + url.substring(url.lastIndexOf('/') + 1);
+	} else {
+		location.href = "../" + url.substring(url.lastIndexOf('/') + 1);
 
-	function menuFunction() {
-		if (x.className === "topnav") {
-		  x.className += " responsive";
-		} else {
-		  x.className = "topnav";
-		}
-	  }
-
-	  function countryHandler(value){
-			if (value=="gallego"){
-				location.href = "gl/"+url.substring(url.lastIndexOf('/')+1);
-			} else {
-				location.href = "../"+url.substring(url.lastIndexOf('/')+1);
-
-			}
-	  }
+	}
+}
